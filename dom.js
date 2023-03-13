37201b58c9360becbef3a0fe22224ef0472a1db1
Yash Gautam <gautamyash37@gmail.com>
2023-03-13 14:05:12 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,7 @@
+var headerTitle = document.getElementById('header-title');
+//headerTitle.textContent = 'hello';
+var header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000';
+var addTitle = document.getElementById('main'); // select the element by its ID
+addTitle.style.fontWeight = 'bold';
+addTitle.style.color = 'green';
