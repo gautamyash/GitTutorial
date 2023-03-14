3617271f426e5120e84ca18897ab411d641c4111
Yash Gautam <gautamyash37@gmail.com>
2023-03-14 14:22:21 +0530

    new changes

---
+++ dom.js
@@ -0,0 +1,21 @@
+// var headerTitle = document.getElementById('header-title');
+// //headerTitle.textContent = 'hello';
+// var header = document.getElementById('main-header');
+// header.style.borderBottom = 'solid 3px #000';
+// var addTitle = document.getElementById('main'); // select the element by its ID
+// addTitle.style.fontWeight = 'bold';
+//addTitle.style.color = 'green';
+var items = document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor = 'green';
+
+
+for (var i = 0; i < items.length; i++) {
+  items[i].style.fontWeight = 'bold';
+  
+}
+
+
+
+ 
+
+
