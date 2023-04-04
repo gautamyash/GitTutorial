b3039b9164c0e6ef4003b560215f1d10a44a2c07
gautamyash <gautamyash37@gmail.com>
2023-04-04 19:40:20 +0530

    New Changes

--- Promises and Async.js
+++ Promises and Async.js
@@ -21,3 +21,23 @@ const deletePost = async () => {
     }, 1000);
   });
 };
+const createAndDeletePosts = async () => {
+    try {
+      const post1 = await createPost({ title: "My first post" });
+      console.log(`Created post with title "${post1.title}"`);
+      
+      const post2 = await createPost({ title: "My second post" });
+      console.log(`Created post with title "${post2.title}"`);
+      
+      const deletedPost = await deletePost();
+      console.log(`Deleted post with title "${deletedPost.title}"`);
+      
+      const remainingPosts = posts.map((post) => post.title);
+      console.log(`Remaining posts: ${remainingPosts}`);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  createAndDeletePosts();
+  
